bcf5819d672532279a724de9da23c6040886ba64
sumanthya <118129862+sumanthya@users.noreply.github.com>
2022-11-27 17:46:01 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -10,6 +10,6 @@ def newBuild()
 
 def newDeploy(jobname,ip,appname)
 {
-    sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:var/lib/tomcat9/webapps/${appname}.war"
+    sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${appname}.war"
 }
   
